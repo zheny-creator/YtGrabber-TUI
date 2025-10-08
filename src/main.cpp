508a7c5203b6d81ade630daee5c5176ce6b56998
zheny-creator <rimkamix0@gmail.com>
2025-10-08 14:04:04 +0300

    change version to beta 1

--- src/main.cpp
+++ src/main.cpp
@@ -322,7 +322,7 @@ int main()
             cout << "YtGrabber-TUI" << endl;
             cout << "Надстроками для yt-dlp" << endl;
             cout << "Автор: Женя Бородин" << endl;
-            cout << "Верия: 1.0 Alpha 2" << endl;
+            cout << "Версия: 1.0 Beta" << endl;
         }
         if (choice == 5)
         {
